18b412b1caaf354bb825c4c0ca40cdcf5db87300
ByoungHwi Yoon <1994blyo@gmail.com>
2020-10-28 23:18:52 +0900

    feat : IssueListCollectionViewCell 구현 IssueListView CollectionView의 Cell 구현 마일스톤 Label 및 레이블 커스텀 뷰 구현 필요 Configure 함수 구현 예정

---
+++ ios/IssueTracker/IssueTracker/IssueList/View/IssueListCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  IssueListCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by Byoung-Hwi Yoon on 2020/10/28.
+//
+
+import UIKit
+
+class IssueListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var milestoneLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
