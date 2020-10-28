cb01a28c6eead2cfb173fe179e947c0736e77db4
ByoungHwi Yoon <1994blyo@gmail.com>
2020-10-28 22:00:25 +0900

    feat : IssueListViewController 생성 IssueListView의 ViewController 생성 CollectionView IBOulet 연결

---
+++ ios/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IssueListViewController.swift
+//  IssueTracker
+//
+//  Created by Byoung-Hwi Yoon on 2020/10/28.
+//
+
+import UIKit
+
+class IssueListViewController: UIViewController {
+
+    @IBOutlet weak var issueListCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
