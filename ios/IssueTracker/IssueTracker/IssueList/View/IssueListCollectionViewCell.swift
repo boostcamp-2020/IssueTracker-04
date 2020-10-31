f0f1ba6fcf44f68867da149fc1fc12e6a619cf11
ByoungHwi Yoon <1994blyo@gmail.com>
2020-10-31 17:33:40 +0900

    feat : IssueListCollectionView Cell 수정 다중선택과 삭제 및 이슈닫기를 위한 레이아웃 수정 화면 전환 레이아웃 기능 구현

--- ios/IssueTracker/IssueTracker/IssueList/View/IssueListCollectionViewCell.swift
+++ ios/IssueTracker/IssueTracker/IssueList/View/IssueListCollectionViewCell.swift
@@ -21,23 +21,44 @@ class IssueListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var milestoneLabel: BadgeLabel!
     @IBOutlet var labelContainerView: LabelContainerView!
     
+    @IBOutlet weak var rightContainerView: UIView!
+    @IBOutlet weak var leftContainerView: UIView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var labelContainerViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var containerViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mainViewWidthConstraint: NSLayoutConstraint!
     
     static var identifier: String {
         String(describing: Self.self)
     }
     
-    var cellWidth: CGFloat? {
+    var cellMainWidth: CGFloat? {
         didSet {
-            guard let width = cellWidth else {
+            guard let width = cellMainWidth else {
                 return
             }
             containerView.translatesAutoresizingMaskIntoConstraints = false
-            containerViewWidthConstraint.constant = width
+            mainViewWidthConstraint.constant = width
+            containerViewWidthConstraint.constant = width * 1.5
             labelContainerView.maxWidth = width - 16
+            showMainView()
         }
+    }
+    
+    private var leftContainerViewWidth: CGFloat {
+        (cellMainWidth ?? 0 ) * 0.15
+    }
+    
+    private var rigthContainerViewWidth: CGFloat {
+        (cellMainWidth ?? 0 ) * 0.35
+    }
+    
+    private var leftShowOrigin: CGPoint = .zero
+    private var mainShowOrigin: CGPoint {
+        CGPoint(x: leftContainerViewWidth, y: 0)
+    }
+    private var rightShowOrigin: CGPoint {
+        CGPoint(x: leftContainerViewWidth + rigthContainerViewWidth, y: 0)
     }
     
     var labelRowCount: CGFloat? {
@@ -56,6 +77,36 @@ class IssueListCollectionViewCell: UICollectionViewCell {
         milestoneLabel.text = data.milestoneTitle
         labelContainerView.add(labels: data.labels)
         labelRowCount = CGFloat(labelContainerView.labelRows)
+    }
+    
+    func showLeftContainerView() {
+        bounds.origin = leftShowOrigin
+    }
+    
+    func showMainView() {
+        bounds.origin = mainShowOrigin
+    }
+    
+    func showRightContainerView() {
+        bounds.origin = rightShowOrigin
+    }
+    
+    func leftContainerViewShowAnimate() {
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut) { [weak self] in
+            self?.showLeftContainerView()
+        }
+    }
+    
+    func rightContainerViewShowAnimate() {
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut) { [weak self] in
+            self?.showRightContainerView()
+        }
+    }
+    
+    func resetViewAnimate() {
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut) { [weak self] in
+            self?.showMainView()
+        }
     }
     
     override func prepareForReuse() {
