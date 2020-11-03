e559725edbcb1e6272a766159e432be8bc1244f2
JunYoung Jang <vnslt0152@gmail.com>
2020-11-03 11:55:52 +0900

    feat : loginView component 생성
    component 사용을 위한 input태그 component화

---
+++ client/src/components/loginView/input/index.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useRef } from 'react';
+import './style.scss';
+
+const input = (props) => {
+    const {placeholder, value, type, onChange} = props;
+    // const [value, setValue] = useState("");
+
+    return (
+        <input
+            placeholder = {placeholder || ''}
+            value = {value}
+            type = {type}
+            onChange = {onChange}
+         />
+    );
+};
+
+export default input;
+
