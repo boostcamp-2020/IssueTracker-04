93b970169f38ae6505c00ab8afbeae7eb62688a4
ByoungHwi Yoon <1994blyo@gmail.com>
2020-11-03 12:20:46 +0900

    feat : IssueDetailView 작업 분할을 위한 레이아웃 구성 IssueDetailView와 SlideView 동시 작업을 위한 임시 레이아웃 구성 추후 SlideEffect 수정 필요

---
+++ ios/IssueTracker/IssueTracker/IssueDetail/Controller/IssueDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  IssueDetailViewController.swift
+//  IssueTracker
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/03.
+//
+
+import UIKit
+
+class IssueDetailViewController: UIViewController {
+
+    var gesture = UIPanGestureRecognizer()
+    
+    @IBOutlet weak var slideViewTobConstraint: NSLayoutConstraint!
+    @IBOutlet weak var slideView: IssueDetailSlideView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gesture = UIPanGestureRecognizer(target: self, action: #selector(onAction))
+        slideView.addGestureRecognizer(gesture)
+        
+    }
+    
+    @objc func onAction() {
+        let yLocationTouched = gesture.location(in: self.view).y
+        print("\(yLocationTouched)")
+
+        slideViewTobConstraint.constant = view.frame.height - yLocationTouched
+        view.layoutIfNeeded()
+    }
+}
