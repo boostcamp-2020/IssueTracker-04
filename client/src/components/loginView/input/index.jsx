1e7fd3670dbbcbee5d3cb10ce7e3659498d2c950
jangky000 <jangky000@mme.dongguk.edu>
2020-11-03 21:34:08 +0900

    feat : loginView 디자인
    scss 적용을 위한 loginView 디자인 작업

--- client/src/components/loginView/input/index.jsx
+++ client/src/components/loginView/input/index.jsx
@@ -1,19 +1,11 @@
 import React, { useState, useRef } from 'react';
 import './style.scss';
 
-const input = (props) => {
-  const {
- placeholder, value, type, onChange, } = props;
+const input = props => {
+  const { placeholder, value, type, onChange } = props;
   // const [value, setValue] = useState("");
 
-  return (
-    <input
-      placeholder={placeholder || ''}
-      value={value}
-      type={type}
-      onChange={onChange}
-    />
-  );
+  return <input className="loginInput" placeholder={placeholder || ''} value={value} type={type} onChange={onChange} />;
 };
 
 export default input;
