ed509fe0314dc77fb68cfe72c7ec4fcd423370b3
Oh-Donggeon <donggeon.dev@gmail.com>
2020-11-04 21:37:54 +0900

    feat : 이슈 추가 및 수정 뷰 추가 및 레이아웃 구성 Main.storyboard에서 이슈 뷰 및 레이아웃 구성 사진 업로드 하는 context 메뉴 추가

---
+++ ios/IssueTracker/IssueTracker/AddIssue/View/IssueMarkdownTextView.swift
@@ -0,0 +1,16 @@
+//
+//  IssueMarkdownTextView.swift
+//  IssueTracker
+//
+//  Created by Oh Donggeon on 2020/11/04.
+//
+
+import UIKit
+
+class IssueMarkdownTextView: UITextView {
+    
+    func setIssue(content: String) {
+        text = content
+    }
+    
+}
