811659113b4822ea5a1d4e34637e06139ba1f6ee
ByoungHwi Yoon <1994blyo@gmail.com>
2020-11-04 17:20:51 +0900

    feat : IssueDetailView 데이터 관리 객체 구현 IssueDetailViewCollectionView 데이터소스 관리를 위한 Adapter 구현 데이터 관리를 위한 DataSourceManager 구현

---
+++ ios/IssueTracker/IssueTracker/IssueDetail/Model/IssueDetailDataSourceManager.swift
@@ -0,0 +1,47 @@
+//
+//  IssueDetailDataSourceManager.swift
+//  IssueTracker
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/04.
+//
+
+import Foundation
+
+struct IssueInfo {
+    let issueNo: Int
+    let issueTitle, issueContent: String
+    let issueFlag: Bool
+    let issueDate: Date
+    let issueAuthorNo: Int
+    let issueAuthorID: String
+    
+    init(issue: IssueItem) {
+        issueNo = issue.issueNo
+        issueTitle = issue.issueTitle
+        issueContent = issue.issueContent
+        issueFlag = issue.issueFlag
+        issueDate = issue.issueDate
+        issueAuthorNo = issue.issueAuthorNo
+        issueAuthorID = issue.issueAuthorID
+    }
+}
+
+class IssueDetailDataSourceManager {
+    
+    var detailItem: IssueDetailItem?
+    
+    var issueInfo: IssueInfo? {
+        guard let issue = detailItem?.issue else {
+            return nil
+        }
+        return IssueInfo(issue: issue)
+    }
+    
+    var commentCount: Int {
+        detailItem?.comments.count ?? 0
+    }
+    
+    subscript(indexPath: IndexPath) -> CommentItem? {
+        return detailItem?.comments[indexPath.row]
+    }
+}
