e06be5bc9de809263760aae701ed352768624318
jangky000 <jangky000@mme.dongguk.edu>
2020-11-05 14:40:14 +0900

    feat : GitHub 로그인 로직 변경
    GitHub 회원인증 및 가입을 위한 accessToken 처리 로직 추가

--- server/routes/auth/github.js
+++ server/routes/auth/github.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const gitAuth = require('../../services/auth/github');
 
-router.get('/api/auth/github/', gitAuth.gitLoginCheck);
+// router.get('/api/auth/github/', gitAuth.gitLoginCheck);
 
-router.get(
-  '/api/auth/github/callback',
-  gitAuth.gitLoginCallback,
-  gitAuth.gitRedirect
-);
+// router.get(
+//   '/api/auth/github/callback',
+//   gitAuth.gitLoginCallback,
+//   gitAuth.gitRedirect
+// );
+
+// 라우터 이름 추후 확정하기
+router.post('/api/auth/github/ios', gitAuth.gitIosLogin);
 
 module.exports = router;
