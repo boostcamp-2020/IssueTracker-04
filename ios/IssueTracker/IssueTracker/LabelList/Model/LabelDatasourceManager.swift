e0f3037d2b8960665b51af761736ef8ece7cf8c8
ByoungHwi <1994blyo@gamil.com>
2020-11-08 16:01:58 +0900

    feat : LabelListView DatasourceManager 구현 LabelListView의 Data 관리를 위한 DatasourceManager구현 추가 삭제등 추가적인 로직 작성 필요

--- ios/IssueTracker/IssueTracker/LabelList/Model/LabelDatasourceManager.swift
+++ ios/IssueTracker/IssueTracker/LabelList/Model/LabelDatasourceManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class LabelDatasourceManager {
+    var items: [LabelDetail] = []
+    var itemCount: Int {
+        items.count
+    }
+    
+    subscript(indexPath: IndexPath) -> LabelDetail {
+        items[indexPath.row]
+    }
+    
+    func loadData() {
+        items = DummyDataLoader().loadLabels()
+    }
+}
