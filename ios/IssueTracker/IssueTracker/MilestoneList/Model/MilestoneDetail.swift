4cb393ea70d9e43b0b14f8ae11a51b96ca78841a
ByoungHwi <1994blyo@gamil.com>
2020-11-08 14:59:49 +0900

    feat : MilestoneView Data Model 구현 MilestoneView의 마일스톤 DataModel 구현

---
+++ ios/IssueTracker/IssueTracker/MilestoneList/Model/MilestoneDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MilestoneDetail.swift
+//  IssueTracker
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/08.
+//
+
+import Foundation
+
+struct MilestoneDetail: Codable, MilestoneDetailViewData {
+    var milestoneNo: Int
+    var milestoneTitle: String
+    var dueDate: Date
+    var milestoneDescription: String
+    var percent: Float
+    var openIssueCount: Int
+    var closedIssueCount: Int
+}
