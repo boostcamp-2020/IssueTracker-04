2a3a12e551f90d99a436bf3e5dcc68f28fbf473f
ByoungHwi <1994blyo@gamil.com>
2020-11-08 14:09:19 +0900

    feat : MilestoneListView Collectionview Cell 구현 MilestoneView CollectionView의 Cell 구현 MilestoneDetailView를 구현하여 Cell의 하위뷰로 추가

--- ios/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListCell.swift
+++ ios/IssueTracker/IssueTracker/MilestoneList/View/MilestoneListCell.swift
@@ -5,4 +5,53 @@
 //  Created by Byoung-Hwi Yoon on 2020/11/08.
 //
 
-import Foundation
+import UIKit
+
+class MilestoneListCell: SwipableCollectionViewCell {
+    
+    override func commonInit() {
+        super.commonInit()
+        addMilestoneDetailView()
+        addDeleteButton()
+    }
+    
+    private func addMilestoneDetailView() {
+        guard let mainView = mainView else {
+            return
+        }
+        let milestoneView = MilestoneDetailView()
+        milestoneView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.addSubview(milestoneView)
+        
+        let constraints = [milestoneView.topAnchor.constraint(equalTo: mainView.topAnchor),
+                           milestoneView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor),
+                           milestoneView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor),
+                           milestoneView.trailingAnchor.constraint(equalTo: mainView.trailingAnchor)]
+        NSLayoutConstraint.activate(constraints)
+        
+        milestoneView.titleLabel.text = "HAHAHA"
+    }
+    
+    private func addDeleteButton() {
+        guard let rightContainerView = rightContainerView else {
+            return
+        }
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        rightContainerView.addSubview(button)
+        
+        let constraints = [button.topAnchor.constraint(equalTo: rightContainerView.topAnchor),
+                           button.heightAnchor.constraint(equalTo: rightContainerView.heightAnchor),
+                           button.leadingAnchor.constraint(equalTo: rightContainerView.leadingAnchor),
+                           button.trailingAnchor.constraint(equalTo: rightContainerView.trailingAnchor)]
+        NSLayoutConstraint.activate(constraints)
+        
+        button.backgroundColor = .systemRed
+        button.setTitle("Delete", for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(deleteButtonDidTouched), for: .touchUpInside)
+    }
+    
+    @objc private func deleteButtonDidTouched() {
+    }
+}
