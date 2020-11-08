dd4054555b147bd7681f5e5b04a0d1fea858b874
ByoungHwi <1994blyo@gamil.com>
2020-11-08 14:10:17 +0900

    feat : MilestoneView ViewController 구현 Cell을 화면에 표시하기위한 ViewController 구현 세부기능 추가예정

--- ios/IssueTracker/IssueTracker/MilestoneList/Controller/MilestoneListViewController.swift
+++ ios/IssueTracker/IssueTracker/MilestoneList/Controller/MilestoneListViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class MilestoneListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MilestoneListCell.self, forCellWithReuseIdentifier: MilestoneListCell.identifier)
 
-        // Do any additional setup after loading the view.
+    }
+}
+
+extension MilestoneListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 88)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        1
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 1, left: 0, bottom: 1, right: 0)
+    }
+}
 
+extension MilestoneListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: MilestoneListCell.identifier, for: indexPath)
+    }
 }
